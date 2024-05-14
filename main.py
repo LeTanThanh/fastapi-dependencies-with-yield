926b443863c64fed02e60104b64b90c0764a0626
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-14 15:44:07 +0700

    A dependency with yield and try

--- main.py
+++ main.py
@@ -38,3 +38,21 @@ async def get_db():
   finally:
     db.close()
 """
+
+# A dependency with yield and try
+
+"""
+If you use a try block in a dependency with yield, you'll receive any exception that was thrown when using the dependency.
+For examples, if some code at the some point in the middle, in another dependenct or in a path operation, made a database transaction rollback or create any other error, you will receive the exception in your dependency.
+
+So, you can look that specific exception inside the dependency with except SomeException.
+
+In the same way, you can use finally to make sure the exit steps are executed, no matter if there was an exception or not.
+
+async def get_db():
+  db = DBSession()
+  try:
+    yield db
+  finally:
+    db.close()
+"""
