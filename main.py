9adb8bf025682b30fd932fc80484582ea8dd003b
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-14 15:26:06 +0700

    Dependencies with yield

---
+++ main.py
@@ -0,0 +1,6 @@
+# Dependencies with yield
+"""
+FastAPI supports dependencies that do some extra steps after finishing.
+
+To do this, use yield instead of return, and write the extra steps (code) after.
+"""
